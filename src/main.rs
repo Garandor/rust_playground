c5a1a5ea1461be170377f05629b03ffd67228dd4
Adam Reif <stadamreif@gmail.com>
2021-04-10 19:02:44 -0500

    Build and Coverage on Travis CI (#1)
    * Create .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * generic_add_int w test function
    
    * travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{ops::Add};
+
+fn add_int<'a,'b,T>(x: &'a T,y: &'a T) -> T
+where
+    &'a T: Add<&'a T, Output=T>
+{
+    x + y
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_add(){
+    let a= 1u8;
+    let b= 4u8;
+    assert_eq!(a+b,add_int(&a,&b));
+    let a= 1u64;
+    let b= 4u64;
+    assert_eq!(a+b,add_int(&a,&b));
+    let a= 1f64;
+    let b= 4f64;
+    assert_eq!(a+b,add_int(&a,&b));
 }
